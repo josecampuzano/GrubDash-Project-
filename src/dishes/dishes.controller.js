160dab44115e14d13aaa49122f85d7adb9893c36
Jose Campuzano <campuzanojoseluis14@gmail.com>
2022-09-05 22:05:03 -0700

    dhishes create method complete

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,53 @@
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body
+        if (data[propertyName]) {
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Must include ${propertyName}`
+        })
+    }
+}
+
+function create(req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body
+    const newDish = {
+        id: Number(nextId),
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url,
+    }
+    dishes.push(newDish)
+    res.status(201).json({ data: newDish })
+}
+
+function checkPriceLessThanZero(req, res, next) {
+    const { data: { price } = {} } = req.body
+    if (price >= 0) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: "The price cannot be less than zero(0)"
+    })
+}
+
+
+module.exports = {
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        checkPriceLessThanZero,
+        create,
+    ] 
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller")
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+router.route("/").post(controller.create)
 
 module.exports = router;
