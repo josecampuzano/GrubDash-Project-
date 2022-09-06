b5c4e157c0ea5d6ef227f73d135f192da6bb3fcc
Jose Campuzano <campuzanojoseluis14@gmail.com>
2022-09-06 09:24:29 -0700

    path orders added create functionality; missing some validation

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,68 @@
 const path = require("path");
-
-// Use the existing order data
+const { forEach } = require("../data/orders-data");
 const orders = require(path.resolve("src/data/orders-data"));
-
-// Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body
+        if (data[propertyName]) {
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Must include ${propertyName}`
+        })
+    }
+}
+
+function checkDishesEmpty(req, res, next) { //has res.locals.dishesArray
+    const { ...orders } = req.body
+    const dishesArray = orders.data.dishes
+    res.locals.dishesArray = dishesArray
+    if(dishesArray.length === 0) {
+        next({
+            status: 400,
+            message: `Dishes must include at least one dish`
+        })
+    }
+    return next()
+}
+
+function checkDishesIsAnArray(req, res, next) {
+    let result = Array.isArray(res.locals.dishesArray)
+    if(result === true) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `The dish entry must be an array`
+    })
+}
+
+function create(req, res, next) {
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body
+    const newOrder = { 
+        id: Number(nextId), 
+        deliverTo: deliverTo,
+        mobileNumber: mobileNumber,
+        status: status,
+        dishes: dishes,
+    }
+    orders.push(newOrder)
+    res.status(201).json({ data: newOrder })
+}
+
+
+
+
+module.exports = {
+    create: [
+        bodyDataHas("deliverTo"),
+        bodyDataHas("mobileNumber"),
+        bodyDataHas("dishes"),
+        checkDishesEmpty,
+        checkDishesIsAnArray,
+        create,
+    ]
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const controller = require("./orders.controller")
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+router.route("/").post(controller.create)
 
 module.exports = router;
